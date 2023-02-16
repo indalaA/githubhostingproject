259521e8cded868b5db3567a99356d8cd900566c
Aparna Indala <aparnai@spyhre.local>
2023-02-16 19:20:51 +0530

    git

--- src/app/userdetails/password-strength.validators.ts
+++ src/app/userdetails/password-strength.validators.ts
@@ -12,12 +12,31 @@ export const PasswordStrengthValidator = function (control: AbstractControl): Va
   let lowerCaseCharacters = /[a-z]+/g;
   let numberCharacters = /[0-9]+/g;
   let specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-  if (upperCaseCharacters.test(value) === false || lowerCaseCharacters.test(value) === false || numberCharacters.test(value) === false || specialCharacters.test(value) === false) {
-    return {
-      passwordStrength:'password should contain at least 1 special character, 1 number ,1 lowecase letter, 1 uppercase letter'
-      // passwordStrength: 'Password must contain at least two of the following: numbers, lowercase letters, uppercase letters, or special characters.'
-    }
+    
+if(upperCaseCharacters.test(value)==false)
+{
+  return{
+    passwordStrength:'password should contain at least one uppercase letter'
+  }
+}
+if(lowerCaseCharacters.test(value)==false)
+{
+  return{
+    passwordStrength:'password should contain at least one lowercase letter'
+  }
+}
+if(numberCharacters.test(value)==false)
+{
+  return{
+    passwordStrength:'password should contain at least one number'
 
   }
+}
+if(specialCharacters.test(value)==false)
+{
+  return{
+    passwordStrength:'password should contain at least on special Character'
+  }
+}
 return null
 }
